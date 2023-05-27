a071379d86c9e2d71377e43a06ccc2cd912d0fe1
abdirahman <57835339+Butugey12@users.noreply.github.com>
2023-05-27 17:54:57 +0200

    commit all

---
+++ index.js
@@ -0,0 +1,10 @@
+const fetchData = async() => {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  console.log(data);
+
+  document.getElementById("title").innerHTML=`Advice #${data.slip.id}`;
+  document.getElementById("text").innerHTML = `"${data.slip.advice}"`;
+
+}
+fetchData();
